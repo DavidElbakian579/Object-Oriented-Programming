beb44e59323516745c2776d86156a414c465ab78
David Elbakian <elbakiand@gmail.com>
2021-07-24 16:41:34 +0400

    javasyntaxpro8l7

---
+++ Human.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasyntaxpro8l7;
+
+/**
+ *
+ * @author David-pc
+ */
+public class Human extends Entity {
+    public void speak() {
+        System.out.println("Я умею общаться");
+    }
+}
---
+++ JavaDeveloper.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasyntaxpro8l7;
+
+/**
+ *
+ * @author David-pc
+ */
+public class JavaDeveloper extends Human{
+    public void code() {
+        System.out.println("Я умею общаться на Java");
+    }
+}
